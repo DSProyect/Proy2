b6a13d37f9c5854616d821daae3b722f2a9ef77f
User <leo_silver12@hotmail.com>
2018-01-15 19:54:14 -0500

    alter base

--- SARES.sql
+++ SARES.sql
@@ -1,145 +1,235 @@
-CREATE SCHEMA IF NOT EXISTS `SARES` DEFAULT CHARACTER SET utf8 ;
-USE `SARES` ;
+drop database if exists SARES;
 
-CREATE TABLE IF NOT EXISTS `SARES`.`Usuarios` (
-	`idUsuario` INT(10) NOT NULL AUTO_INCREMENT,
-    `usuario` varchar(25) NOT NULL,
-    `password` varbinary(15) not null,
-    `cargo` varchar(25) not null,
-    `nombre` varchar(25) not null,
-    `sueldo` double,
-    `apellido` varchar(25),
-    `edad` int(10),
-    primary key(`idUsuario`));
-    
-CREATE TABLE IF NOT EXISTS `SARES`.`Categorias` (
-	`idCategoria` INT(10) NOT NULL auto_increment,
-    `nombre` varchar(25) not null,
-    primary key(`idCategoria`));
-    
-CREATE TABLE IF NOT EXISTS `SARES`.`Plato` (
-	`idPlato` INT(10) NOT NULL AUTO_INCREMENT,
-    `nombre` varchar(25) not null,
-    `Precio` Double not null,
-    `idCategoria` INT(10) not null,
-    `tiempoEstimado`time,
-    `cantidad` int(10),
-    primary key(`idPlato`),
-    index(`idCategoria`),
-    FOREIGN KEY(`idCategoria`) REFERENCES `Categorias`(idCategoria)  );
-    
-CREATE TABLE IF NOT EXISTS `SARES`.`BEBIDAS` (
-	`idBebida` INT(10) NOT NULL auto_increment,
-    `nombre` varchar(25) not null,
-    `precio` double not null,
-    `litros` double not null,
-    `cantidad` int(10),
-    `idCategoria` int(10) not null,
-    PRIMARY KEY(`idBebida`),
-    index(`idCategoria`),
-    FOREIGN KEY(`idCategoria`) REFERENCES `Categorias`(idCategoria)  );
-    
-CREATE TABLE IF NOT EXISTS `SARES`.`Pedido` (
-	`idPedido` int(10) NOT NULL auto_increment,
-    `horaIngreso` datetime,
-    `tiempoEstimado` time,
-    `estado` varchar(25),
-    `observaciones` varchar(50),
-    PRIMARY KEY(`idPedido`));
-    
-CREATE TABLE IF NOT EXISTS `SARES`.`Mesa` (
-	`idMesa` INT(10) NOT NULL auto_increment,
-    `tratoEspecial` bool,
-    `cantSillas` int(10),
-    `ambiente` varchar(25) not null,
-    primary key(`idMesa`));
-    
-CREATE TABLE IF NOT EXISTS `SARES`.`Combos` (
-	`idCombo` INT(10) NOT NULL auto_increment,
-    `nombre` varchar(25) not null,
-    `tiempoEstimado` time,
-    PRIMARY KEY(`idCombo`));
-    
-CREATE TABLE IF NOT EXISTS `SARES`.`CombosPlato`(
-	`idCombo` INT(10) NOT NULL auto_increment,
-    `idPlato` INT(10) not null,
-    index(`idCombo`,`idPlato`),
-    foreign key(`idCombo`) references `Combos`(`idCombo`),
-    foreign key(`idPlato`) references `Plato`(`idPlato`));
-    
-CREATE TABLE IF NOT EXISTS `SARES`.`CombosBebidas`(
-	`idCombo` INT(10) NOT NULL auto_increment,
-    `idBebida` int(10) not null,
-    index(`idCombo`,`idBebida`),
-    foreign key(`idCombo`) references `Combos`(`idCombo`),
-    foreign key(`idBebida`) references `Bebidas`(`idBebida`));
-    
-CREATE TABLE IF NOT EXISTS `SARES`.`Cuentas` (
-	`idCuenta` int(10) not null auto_increment,
-    `tratoEspecial` bool,
-    primary key(`idCuenta`));
-    
-CREATE TABLE IF NOT EXISTS `SARES`.`Ingredientes` (
-	`idIngrediente` int(10) not null auto_increment,
-    `nombre` varchar(25) not null,
-    `cantidad` int(10) not null,
-    primary key(`idIngrediente`));
-    
-CREATE TABLE IF NOT EXISTS `SARES`.`PlatosIngredientes` (
-	`idPlato` int(10) not null,
-    `idIngrediente` int(10) not null,
-    index(`idPlato`,`idIngrediente`),
-    foreign key(`idPlato`) references `plato`(`idPlato`),
-    foreign key(`idIngrediente`) references `ingredientes`(`idIngrediente`));
-    
-CREATE TABLE IF NOT EXISTS `SARES`.`Cliente` (
-	`idCliente` int(10) not null auto_increment,
-    `nombre` varchar(25) not null,
-    `apellido` varchar(25) not null,
-    `edad` int(8) not null,
-    primary key(`idCliente`));
+create database if not exists SARES;
 
-CREATE TABLE IF NOT EXISTS `SARES`.`MesaCuenta` (
-	`idCuenta` int(10) not null,
-    `idMesa` int(10) not null,
-    index(`idCuenta`,`idMesa`),
-    foreign key(`idCuenta`) references `cuentas`(`idCuenta`),
-    foreign key(`idMesa`) references `mesa`(`idMesa`));
+use SARES;
+
+CREATE TABLE Usuario(
+	usuario varchar(50) not null,
+    clave varchar(50) not null,
+    primary key (usuario)
+);
+
+CREATE TABLE Rol(
+	idRol int not null auto_increment,
+    nombreRol varchar(50) not null,
+    eliminado boolean not null,
+    primary key (idRol)
+);
+
+CREATE TABLE Empleado(
+	cedula varchar(10) not null,
+    nombres varchar(255) not null,
+    apellidos varchar(255) not null,
+    edad int not null,
+    sueldo float not null,
+    idCargo int not null auto_increment,
+    usuario varchar(50) not null,
+    eliminado boolean not null,
+    primary key (cedula),
+    foreign key (usuario) references Usuario(usuario),
+    foreign key (idRol) references Cargo(idRol)
+);
+
+CREATE TABLE Environments(
+	idENvi int not null auto_increment,
+    nombre varchar(255) not null,
+    eliminado boolean not null,
+    primary key (idEnvi)
+);
+
+CREATE TABLE Mesa(
+	idMesa int not null auto_increment,
+    asientos int not null,
+    disponibilidad boolean not null,
+    idEnvi int not null,
+    eliminado boolean not null,
+    Primary Key (idMesa),
+    foreign key (idEnvi) references Environments(idEnvi)
+);
+
+CREATE TABLE Cliente (
+	ced varchar(10) not null,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Direccion varchar(255),
+    eliminado boolean not null,
+    PRIMARY KEY (ced)
+);
+
+CREATE TABLE Categoria_Item(
+	ID int NOT NULL,
+    Nombre VARCHAR(255),
+    Descripcion VARCHAR(255),
     
-CREATE TABLE IF NOT EXISTS `SARES`.`CuentaPedido` (
-	`idCuenta` int(10) not null,
-    `idPedido` int(10) not null,
-    index(`idCuenta`,`idPedido`),
-    foreign key(`idCuenta`) references `cuentas`(`idCuenta`),
-    foreign key(`idPedido`) references `pedido`(`idPedido`));
+    PRIMARY KEY(ID)
+);
+
+CREATE TABLE Item(
+
+	ID int NOT NULL,
+    Nombre VARCHAR(255),
+    Descripcion VARCHAR(255),
     
-CREATE TABLE IF NOT EXISTS `SARES`.`ClienteCuenta` (
-	`idCliente` int(10) not null,
-    `idCuenta` int(10) not null,
-    `descuento` int(8) not null,
-    `tipopago` varchar(25) not null,
-    `fechaCuenta` datetime not null,
-    index(`idCliente`,`idCuenta`),
-    foreign key(`idCuenta`) references `cuentas`(`idCuenta`),
-    foreign key(`idCliente`) references `cliente`(`idCliente`));
+    Precio DOUBLE not null,
+    Disponibilidad boolean not null,
+	TiempoPreparacion int not null,
+    IdCategoria int not null,
+    eliminado boolean not null,
     
-CREATE TABLE IF NOT EXISTS `SARES`.`MeseroPedido` (
-	`idUsuario` INT(10) NOT NULL,
-    `idPedido` INT(10) NOT NULL,
-    index(`idUsuario`,`idPedido`),
-    foreign key(`idUsuario`) references `usuarios`(`idUsuario`),
-    foreign key(`idPedido`) references `pedido`(`idPedido`));
+    PRIMARY KEY (ID),
+    FOREIGN KEY (IdCategoria) references Categoria_Item(ID)
+);
+
+CREATE TABLE Orden (
+	id int not null auto_increment,
+    pagado boolean not null,
+    enPreparacion boolean not null,
+    cocinado boolean not null,
+    entregado boolean not null,
+    idCliente varchar(10) not null,
+    idMesero varchar(10) not null,
+    idChef varchar(10) not null,
     
-CREATE TABLE IF NOT EXISTS `SARES`.`CocineroPedido` (
-	`idUsuario` Int(10) not null,
-    `idPedido` int(10) not null,
-    index(`idUsuario`,`idPedido`),
-    foreign key(`idUsuario`) references `usuarios`(`idUsuario`),
-    foreign key(`idPedido`) references `pedido`(`idPedido`));
+    PRIMARY KEY (id),
+    FOREIGN KEY (idCliente) REFERENCES Cliente(cedula),
+    FOREIGN KEY (idMesero) REFERENCES Personal(cedula),
+    FOREIGN KEY (idChef) REFERENCES Personal(cedula)
+);
+
+CREATE TABLE Detalle_Orden(
+
+	ID_detalle int NOT NULL auto_increment,
+    ID_Orden int NOT NULL,
+    ID_Item int NOT NULL,
+    cantidad int,
+    Observaciones varchar(255),
     
-CREATE TABLE IF NOT EXISTS `SARES`.`CajeroPedido` (
-	`idUsuario` Int(10) not null,
-    `idPedido` int(10) not null,
-    index(`idUsuario`,`idPedido`),
-    foreign key(`idUsuario`) references `usuarios`(`idUsuario`),
-    foreign key(`idPedido`) references `pedido`(`idPedido`));
+    
+    PRIMARY KEY (ID_detalle),
+    FOREIGN KEY (ID_Orden) references Orden(id),
+    foreign key (ID_Articulo) references Articulo(ID)
+);
+
+CREATE TABLE MesaOrden(
+	id int not null auto_increment,
+    idMesa int not null,
+    idOrden int not null,
+    fecha Date,
+    hora time,
+    
+    primary key (id),
+    foreign key (idMesa) references Mesa(idMesa),
+    foreign key (idOrden) references Orden(id)
+);
+
+CREATE TABLE TipoDePago(
+	ID int,
+    Tipo varchar(30),
+    
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Factura (
+    ID int NOT NULL auto_increment,
+    TOTAL double,
+    Fecha DATE,
+    Id_cliente varchar(10) not null,
+    Descuento int,
+    TipoDePago int,
+    
+    PRIMARY KEY (ID),
+    FOREIGN KEY (Id_cliente) REFERENCES Cliente(cedula),
+    FOREIGN KEY (TipoDePago) REFERENCES TipoDePago(ID)
+);
+
+CREATE TABLE Detalle_Factura(
+	id_detalle int,
+    id_factura int,
+    id_orden int,
+    PRIMARY KEY (id_detalle),
+    FOREIGN KEY (id_factura) REFERENCES Factura(ID),
+    FOREIGN KEY (id_orden) REFERENCES Orden(ID)
+);
+delimiter $$
+
+create procedure buscarUsuario(in cadena varchar(255))
+begin
+	select *
+    from Usuario
+    where usuario = cadena;
+end$$
+
+create procedure verificarUsuarioContrasena(in inUsuario varchar(255), in inContrasena varchar(255))
+begin
+	select *
+    from Usuario
+    where usuario = inUsuario and clave = inContrasena;
+end$$
+
+create procedure ObtenerEmpleadoPorUsuario(in cadena varchar(255))
+begin
+	select *
+    from Empleado
+    where usuario = cadena;
+end$$
+
+delimiter ;
+INSERT INTO Rol (idRol, nombreRol, eliminado) 
+VALUES 	(1, 'Administrador', 0), 
+		(2, 'Cocinero', 0), 
+		(3, 'Mesero', 0), 
+        (4, 'Repartidor', 0), 
+        (5, 'Cajero', 0);
+
+
+INSERT INTO Categoria_Item (ID, Nombre) 
+VALUES	(1, 'Entrada'), 
+		(2, 'Segundo'), 
+        (3, 'Postre'), 
+        (4, 'Refresco'), 
+        (5, 'Natural'),
+        (6, 'Artificial');
+
+INSERT INTO TipoDePago (ID, Tipo) 
+VALUES 	(1, 'Efectivo'),  
+		(2, 'Tarjeta de Credito'), 
+        (3, 'Dinero Electronico');
+
+INSERT INTO Environments (idEnvi, nombre, eliminado) 
+VALUES 	(1, 'Normal', 0), 
+		(2, 'VIP', 0);
+
+INSERT INTO Mesa (idMesa, asientos, disponibilidad, idEnvi, eliminado) 
+VALUES 	(1, 2, 1, 1, 0), 
+		(2, 2, 1, 1, 0), 
+		(3, 2, 1, 2, 0), 
+        (4, 4, 1, 1, 0), 
+        (5, 4, 1, 2, 0), 
+        (6, 6, 1, 1, 0), 
+        (7, 8, 1, 1, 0);
+
+INSERT INTO Usuario (usuario, clave)
+VALUES 	('admin', 'superclave'), 
+		('cajero', '1234'), 
+        ('repartidor', '0987'), 
+        ('mesero_1', 'sssd123'),
+        ('mesero_2', '739873'),
+        ('chef', '125');
+
+INSERT INTO Empleado (cedula, nombres, apellidos, edad, sueldo, idRol, usuario, eliminado) 
+VALUES 	('0900000000', 'Luis', 'Arizaga', 23, 500, 1, 'admin', 0),
+		('0921234567', 'Carlos Andres', 'Garcia Zambrano', 25, 350, 2, 'elcocinero', 0),
+        ('0987654321', 'Alberto Luis', 'Andrade Li', 75, 300, 4, 'elrepartidor', 0), 
+        ('0912873456', 'Estefany karen', 'Falconi Seto', 27, 300, 3, 'elmesero1', 0),
+        ('0981237645', 'Jose Jose', 'Primero Segundo', 19, 300, 3, 'elmesero2', 0),
+        ('0987612345', 'Leonardo', 'Velez', 23, 325, 5, 'elcajero', 0);
+
+INSERT INTO Item (ID, Nombre, Descripcion, Precio, Disponibilidad, TiempoPreparacion, IdCategoria, eliminado) 
+VALUES 	(1, 'limonada', 'jarra de limonada ', 4, 1, 3, 5, 0),
+		(2, 'Jugo de Durazno', 'Jugo Artificial', 1, 1, 0, 6, 0),
+		(3, 'Patacones', 'patacones con queso', 2, 1, 4, 1, 0),
+        (4, 'seco de pollo', 'seco de pollo', 4, 1, 10, 2, 0),
+        (5, 'Arroz marinero', 'arroz marinero', 7, 1, 13, 2, 0),
+        (6, 'tres leches', 'tres leches', 3, 1, 4, 3, 0);
+
